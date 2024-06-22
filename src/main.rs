5ae6b6d88766f6964f7bd6696b8f1dfbb4acd6ff
shawnharmsen <shawn@zksnarks.xyz>
2024-06-22 17:04:43 +0900

    shadowing!

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::io;
 
 fn main() {
-    let mut x = 5;
+    let x = 5;
     println!("The secret number is: {}", x);
-    let x = 6;
+    let x = "six";
     println!("The secret number is: {}", x);
 
-    const NUMBERS: [i32; 10] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const NUMBERS: [i32; 10] = [100_000, 200_000, 3, 4, 5, 6, 7, 8, 9, 10];
     for number in NUMBERS {
         println!("The secret number is: {}", number);
     }
