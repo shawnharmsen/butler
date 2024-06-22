f34ea4b3021fcf3901552275cf8ee08a1ea9fa44
shawnharmsen <shawn@zksnarks.xyz>
2024-06-22 16:58:45 +0900

    fun with mut

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,9 @@
 use std::io;
-use std::cmp::Ordering;
-use rand::Rng;
-use colored::*;
 
 fn main() {
-    println!("Enter number");
-
-    let secret = rand::thread_rng().gen_range(1..100);
-
-    println!("The secret number is: {}", secret);
-
-    loop {
-        let mut guess = String::new();
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Not a number");
-                continue;
-            }
-        };
-
-        println!("You entered: {}", guess);
-
-        match guess.cmp(&secret) {
-            Ordering::Less => println!("{}", "Too small!".red()),
-            Ordering::Greater => println!("{}", "Too big!".red()),
-            Ordering::Equal => {
-                println!("{}", "You win!".green());
-                break;
-            }
-        }
-    }
+    let mut x = 5;
+    println!("The secret number is: {}", x);
+    let x = 6;
+    println!("The secret number is: {}", x);
 
 }
