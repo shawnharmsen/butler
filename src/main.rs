b5eebdaf063363cd7b9b9c4b2ef160e085798975
shawnharmsen <shawn@zksnarks.xyz>
2024-06-22 17:02:17 +0900

    testing

--- src/main.rs
+++ src/main.rs
@@ -6,4 +6,8 @@ fn main() {
     let x = 6;
     println!("The secret number is: {}", x);
 
+    const NUMBERS: [i32; 10] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    for number in NUMBERS {
+        println!("The secret number is: {}", number);
+    }
 }
