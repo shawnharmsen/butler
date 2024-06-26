d197c42e39b4bdc87680cbc4b488787fb0a5eb56
shawnharmsen <shawn@zksnarks.xyz>
2024-06-26 15:36:28 +0900

    reinit

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,6 @@
-use reqwest::Error;
-use serde::{Deserialize};
+#![allow(non_snake_case)]
 
-#[derive(Deserialize, Debug)]
-struct TimeApiResponse {
-    datetime: String,
-}
 
-#[tokio::main]
-async fn main() -> Result<(), Error> {
-    let url = "https://worldtimeapi.org/api/timezone/Etc/UTC";
-    let response = reqwest::get(url).await?;
-    let time_api_response: TimeApiResponse = response.json().await?;
-    println!("The current time is: {}", time_api_response.datetime);
-    Ok(())
+fn main() {
+
 }
